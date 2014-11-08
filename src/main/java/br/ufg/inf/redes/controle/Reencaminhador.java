49222a15c0361bdd64e675a090421475665e4353
Daniel Melo <danielmelogpi@gmail.com>
2014-11-08 19:34:37 -0200

    Reencaminhador implementado usando a biblioteca javax.email

--- src/main/java/br/ufg/inf/redes/controle/Reencaminhador.java
+++ src/main/java/br/ufg/inf/redes/controle/Reencaminhador.java
@@ -1,14 +1,70 @@
 package br.ufg.inf.redes.controle;
 
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import br.ufg.inf.redes.entidades.Email;
 
 public class Reencaminhador {
 	
+	//autenticação do Yahoo
+	private String username = "testedeenvioemail@yahoo.com";
+	private String password = "ZGFuaWVsbWVsb2dwaUBnbWFpbC5jb20=";
+	private String yahooHost = "smtp.mail.yahoo.com";
+
 	public boolean encaminharParaOutroSMTP(Email email) {
+
+		  String to = email.getDestinatario();//change accordingly
+	      String from = email.getRemetente();//change accordingly
+	      Properties props = getProperties();
+
+	      // Get the Session object.
+	      Session session = Session.getInstance(props,
+	      new javax.mail.Authenticator() {
+	         protected PasswordAuthentication getPasswordAuthentication() {
+	            return new PasswordAuthentication(username, password);
+	         }
+	      });
+	      
+	      session.setDebug(true);
+
+	      try {
+	         Message message = getMessage(email, to, from, session);
+	         Transport.send(message);
+	         return true;
+
+	      } catch (MessagingException e) {
+	            return false;
+	      }
 		
+	}
+
+	private Message getMessage(Email email, String to, String from, Session session) 
+	throws MessagingException, AddressException {
 		
+		Message message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(from));
+		message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+		message.setSubject(email.getAssunto());
+		message.setText(email.getMensagem());
 		
-		
-		return false;
+		return message;
+	}
+
+	private Properties getProperties() {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", yahooHost);
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.port", "587");
+		return props;
 	}
 }
--- src/main/test/ReencaminhadorTest.java
+++ src/main/test/ReencaminhadorTest.java
@@ -3,28 +3,34 @@ package main.test;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
 import org.junit.Test;
 
 import br.ufg.inf.redes.controle.Reencaminhador;
 import br.ufg.inf.redes.entidades.Email;
 
 public class ReencaminhadorTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
 	
 	@Test
 	public void reencaminharTest() {
 		Reencaminhador reencaminhador = new Reencaminhador();
 		Email email = new Email();
-		email.setRemetente("danielmelogpi@gmail.com");
+		email.setRemetente("testedeenvioemail@yahoo.com");
 		email.setDestinatario("danielmelogpi@gmail.com");
 		email.setAssunto("Assunto do email" + Math.random());
-		email.setMensagem("Minha mensagem " + (Math.PI * Math.random()));
+		email.setMensagem("Minha mensagem " + getLoremIpsum());
 		boolean envio = reencaminhador.encaminharParaOutroSMTP(email);
 		assertTrue(envio);
+	}
+
+	private String getLoremIpsum() {
+		return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut "
+				+ "labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco "
+				+ "laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in "
+				+ "voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non"
+				+ " proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 	}
 
 }
